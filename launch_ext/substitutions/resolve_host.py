6ea1a4f363cab5cf97661a07cad6388a2f194aeb
Russ <russ.webber@greenroomrobotics.com>
2026-05-28 11:16:05 +1000

    fix: ResolveHost should only accept IPv4 (#24)

--- launch_ext/substitutions/resolve_host.py
+++ launch_ext/substitutions/resolve_host.py
@@ -10,17 +10,26 @@ class ResolveHost(Substitution):
         self.__logger = launch.logging.get_logger("launch.user")
 
     def perform(self, context):
+        # Note this only works for IPv4 addresses
         interfaces = psutil.net_if_addrs()
+        ip = None
         if self.__host in interfaces:
-            interface = interfaces[self.__host][0]
-            ip = interface.address
-        else:
-            ip = socket.gethostbyname(self.__host)
+            for iface in interfaces[self.__host]:
+                if iface.family == socket.AF_INET:
+                    ip = iface.address
+                    break
+
+        if ip is None:
+            try:
+                ip = socket.getaddrinfo(self.__host, None, socket.AF_INET)[0][4][0]
+            except socket.gaierror as e:
+                self.__logger.warn(f"Failed to resolve host '{self.__host}': {e}")
+
         self.__logger.info(f"Resolved {self.__host} to {ip}")
         return ip
 
     def describe(self):
-        return f"ResolveHost(interface={self.__node_level.describe()})"
+        return f"ResolveHost(interface={self.__host})"
 
     def describe_condition(self, condition):
         return self.describe()
